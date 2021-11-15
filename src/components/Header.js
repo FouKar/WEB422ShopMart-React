82258ace0b565540c18a41cc6f3100ea66883c31
fkarim20 <fouzan.mdkarim@gmail.com>
2021-11-14 20:40:38 -0500

    Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import {
   Navbar,
   Nav,
@@ -13,32 +13,30 @@ export const Header = () => {
   return (
     <Navbar bg="transparent" expand="lg" className="mx-auto">
       <Container>
-        <Link to="/">
-          <Navbar.Brand>
-            <img
-              height="150"
-              width="150"
-              src="/logo.svg"
-              className="d-inline-block align-top"
-              alt="ShopMart logo"
-            />
-          </Navbar.Brand>
-        </Link>
+        <Navbar.Brand href="/">
+          <img
+            height="150"
+            width="150"
+            src="/logo.svg"
+            className="d-inline-block align-top"
+            alt="ShopMart logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="">
           <Nav className=" headerLink  mx-auto">
-            <Link to="/">
-              <Nav.Link className="mx-auto navLinkText">Home</Nav.Link>
-            </Link>
+            <Nav.Link href="" className="mx-auto navLinkText">
+              Home
+            </Nav.Link>
             <Nav.Link href="" className="mx-auto navLinkText">
               Products
             </Nav.Link>
             <Nav.Link href="#link" className="mx-auto navLinkText">
               Categories
             </Nav.Link>
-            <Link to="/register">
-              <Nav.Link className="mx-auto navLinkText">Registration</Nav.Link>
-            </Link>
+            <Nav.Link href="/register" className="mx-auto navLinkText">
+              Registration
+            </Nav.Link>
             <Form className="d-flex  mx-auto">
               <FormControl
                 type="search"

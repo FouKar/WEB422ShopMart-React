abe25d26bf1a1d3c93c98b1535c0954b3f01a9b8
fkarim20 <fouzan.mdkarim@gmail.com>
2021-11-17 01:12:42 -0500

    product List

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,54 @@
 import React, { useEffect, useContext } from "react";
 import axios from "axios";
 import ShopContext from "../context/ShopContext";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import { Container } from "react-bootstrap";
 
 const ProductList = () => {
   const { products, setProducts } = useContext(ShopContext);
   useEffect(() => {
-    axios(``);
+    axios(`${process.env.REACT_APP_BACK_END_API_DOMAIN}/products/all`)
+      .then((res) => res.data)
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .catch((err) => console.log(err));
   }, []);
-  return <div></div>;
+  return (
+    <Container className="d-flex flex-wrap flex-row">
+      {products.map((product, index) => {
+        return (
+          <Card sx={{ maxWidth: 345 }} className="mx-auto mb-5">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="345"
+                image={product.photo}
+                alt={product.name}
+              />
+              <CardContent className="text-center">
+                <Typography variant="h6" color="text.secondary">
+                  {product.name}
+                </Typography>
+                <Typography
+                  gutterBottom
+                  variant="h6"
+                  component="div"
+                  className="priceCard"
+                >
+                  ${product.price}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        );
+      })}
+    </Container>
+  );
 };
 
 export default ProductList;
--- src/pages/ProductListing.js
+++ src/pages/ProductListing.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ProductList from "../components/ProductList";
 
 const productListing = () => {
   return (
     <>
       <Header />
-      
+      <ProductList />
       <Footer />
       );
     </>
